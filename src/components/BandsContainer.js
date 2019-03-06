df226d88fd5dde0ad98052a4396cf5e2b409427e
github username <github email address>
2019-03-06 18:50:59 -0500

    Done.

--- src/components/BandsContainer.js
+++ src/components/BandsContainer.js
@@ -24,7 +24,7 @@ const mapStateToProps = ({ bands }) => ({ bands })
 
 const mapDispatchToProps = dispatch => ({
   addBand: name => dispatch({ type: "ADD_BAND", name }),
-  delete:  name => dispatch({ type: "DELETE_BAND", name })
+  delete:  id => dispatch({ type: "DELETE_BAND", id })
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(BandsContainer)
